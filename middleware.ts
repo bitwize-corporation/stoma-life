7ee496b11fbb893e0f80ed08b0a3b4763ff3b600
Voicu Bogdan <voicubogdan72@gmail.com>
2024-01-17 15:22:43 +0200

    try1

--- middleware.ts
+++ middleware.ts
@@ -4,3 +4,5 @@ export const config = {
   matcher: ["/adauga-pacient", "/your-pacients", "/add-programare", "/home"],
   //matcher: ["/((?!register|api|login).*)"],
 };
+
+//AWS_ROLE_ARN: arn:aws:iam::538122504114:role/bitwize-stoma
