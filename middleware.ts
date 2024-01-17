c17c55639e5d0ecbb0c415647b4d219bdf3dcc95
Voicu Bogdan <voicubogdan72@gmail.com>
2024-01-17 15:51:50 +0200

    try3

--- middleware.ts
+++ middleware.ts
@@ -4,3 +4,5 @@ export const config = {
   matcher: ["/adauga-pacient", "/your-pacients", "/add-programare", "/home"],
   //matcher: ["/((?!register|api|login).*)"],
 };
+//arn:aws:iam::538122504114:role/bitwize-stoma
+//arn:aws:iam::538122504114:oidc-provider/token.actions.githubusercontent.com
